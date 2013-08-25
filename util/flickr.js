70305c727187d596bddad56ff3bd04f58c1c0253
brian <brianscipione@gmail.com>
2013-08-25 01:02:02 +0000

    flickr returns were getting clobbered at times as a result of trying to or too many tags together and crashing the app; catching error for now, will try to limit tags in future before sending request to flickr

--- util/flickr.js
+++ util/flickr.js
@@ -15,7 +15,7 @@ var APIFlickr = (function () {
             url += '&api_key='+API_KEY;
             url += '&min_taken_date='+params.min_time;
             url += '&max_taken_date='+params.max_time;
-            url += '&tags='+encodeURIComponent(params.performer+','+params.venue_name);
+            url += '&tags='+encodeURIComponent(params.performers+','+params.venue_name);
             url += '&extras=url_s,url_m,url_l';
             url += '&per_page=200';
             url += '&media=photos';
@@ -24,20 +24,22 @@ var APIFlickr = (function () {
         var images = [];
         console.log("APIFlickr:  "+url);
         request.get({url: url, json: true}, function(error, resp, data) {
-            data.photos.photo.forEach(function (img, i) {
-                var curimg = {
-                    'songkick_id':  params.songkick_id,
-                    'songkick_event_id':  params.songkick_event_id,
-                    'flickr_id':  img.id,
-                    'owner':  img.owner,
-                    'url':  'http://www.flickr.com/photos/'+img.owner+'/'+img.id,
-                    'url_s':  img.url_s,
-                    'url_m':  img.url_m,
-                    'url_l':  img.url_l,
-                };
+            if (data.photos !== undefined) {
+                data.photos.photo.forEach(function (img, i) {
+                    var curimg = {
+                        'songkick_id':  params.songkick_id,
+                        'songkick_event_id':  params.songkick_event_id,
+                        'flickr_id':  img.id,
+                        'owner':  img.owner,
+                        'url':  'http://www.flickr.com/photos/'+img.owner+'/'+img.id,
+                        'url_s':  img.url_s,
+                        'url_m':  img.url_m,
+                        'url_l':  img.url_l,
+                    };
 
-                images.push(curimg);
-            });
+                    images.push(curimg);
+                });
+            }
             
             cb(JSON.stringify(images));
         });
